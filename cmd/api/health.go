c68bc3ea9cb7e8cc0b01e54146d2020e7365070d
Michael Homan <mhoman2@stumail.jccc.edu>
2025-01-26 21:11:05 -0600

    Implement json.go for json reading and writing

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -7,6 +7,17 @@ import (
 )
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("ok"))
+
+	data := map[string]string{
+		"status":  "available",
+		"env":     app.config.env,
+		"version": version,
+	}
+
+	if err := writeJSON(w, http.StatusOK, data); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+	}
+
 	app.store.Posts.Create(r.Context(), &postgres.Post{})
+
 }
